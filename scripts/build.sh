0051a40dea57c26c4d5d5bd94a46cf8e1bb07074
Stephen Nixon <swr410@gmail.com>
2018-10-19 17:34:51 -0400

    update file

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@
 
 ###### set vars ######
 
-glyphsSource="sources/experiments/Signika-MM-ext_wght_ext_grad.glyphs"
+glyphsSource="sources/Signika-MM-ext_wght_ext_grad.glyphs"
 
 fontName="Signika-VF"
 
