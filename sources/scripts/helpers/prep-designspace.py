2b1feb6a68816909cca7ad982789ed3ecbc08dc6
Stephen Nixon <swr410@gmail.com>
2018-12-14 10:53:19 -0500

    working through designspace-prepping remote Glyphs script

--- sources/scripts/helpers/prep-designspace.py
+++ sources/scripts/helpers/prep-designspace.py
@@ -1,14 +1,22 @@
+"""
+    Makes a copy of a GlyphsApp source, with adjustments to designspace by:
+    - making masters from "corner" instances
+    - setting up new variable font axes
+    - setting values in new masters to make a rectangular, 2-axis designspace.
+
+    Specific to Signika for now.
+"""
+
 from Glyphs import *
 import sys
 import os
 import objc
 
 relPath = sys.argv[-1]
-# font = GSFont(filename)
 directory = os.getcwd()
 fullPath = str(directory + "/" + relPath)
-# document = Glyphs.open((fullPath), False)
-# font = document.font()
+buildPath = fullPath.replace(".glyphs", "-build.glyphs")
+
 doc = Glyphs.openDocumentWithContentsOfFile_display_(fullPath, False)
 font = doc.font()
 
@@ -34,84 +42,67 @@ negativeWeights = sorted(negativeWeights)
 lightNegValue = negativeWeights[0]
 boldNegValue = negativeWeights[-1]
 
-print(lightValue, boldValue)
-print(lightNegValue, boldNegValue)
-
 # ============================================================================
 # make masters from instance designspace corners =============================
 
+print("Copying glyphs from interpolated corner instances:")
+
+def copyFromInterpolatedFont(instanceIndex):
+    instanceFont = font.generateInstance_error_(font.instances()[instanceIndex], None)
+    instanceFontMasterID = instanceFont.fontMasters()[0].id()
+    font.addFontMaster_(instanceFont.fontMasters()[0])
+
+    print("\n=================================")
+    print("Instance Weight: " + str(font.instances()[instanceIndex].interpolationWeight()))
+
+    for index,glyph in enumerate(font.glyphs()[:10]): #first 10 glyphs only, while making/testing script
+        print(". ", end="", flush=True)
+        # make variable for glyph of interpolated font
+        instanceGlyph = instanceFont.glyphs()[index]
+        instanceFontLayer = instanceGlyph.layerForKey_(instanceFontMasterID)
+        glyph.setLayer_forKey_(instanceFontLayer, instanceFontMasterID)
+
+#     # # bring kerning in from interpolated font
+#     # in Python API: currentFont.kerning[newMasterID] = instanceFont.kerning[newMasterID]
+#     # font.setKerning_(instanceFont.fontMasters()[0].kerning(), instanceFontMasterID)
+
 for index, instance in enumerate(font.instances()):
     if "SC" not in str(instance.customValueForKey_("familyName")):
-        if instance.interpolationWeight() == lightValue:
-            print(instance.name())
-            print(instance.interpolationWeight())
-
-            currentLightFont = font.generateInstance_error_(font.instances()[index], None)
-
-            currentLightFontMasterID = currentLightFont.fontMasters()[0].id()
-
-            font.addFontMaster_(currentLightFont.fontMasters()[0])
-
-            for index,glyph in enumerate(font.glyphs()):
-                # make variable for glyph of interpolated font
-                instanceGlyph = currentLightFont.glyphs()[index
-                
-                print(glyph.layerForKey_(currentLightFontMasterID).paths())
-                print(instanceGlyph.layerForKey_(currentLightFontMasterID).paths())
-
-                lightFontLayer = instanceGlyph.layerForKey_(currentLightFontMasterID)
-
-                glyph.setLayer_forKey_(lightFontLayer, currentLightFontMasterID)
+        instanceWeight = instance.interpolationWeight()
+        if instanceWeight == lightValue or instanceWeight == boldValue or instanceWeight == lightNegValue or instanceWeight == boldNegValue:
+            copyFromInterpolatedFont(index)
 
 
-                # # bring kerning in from interpolated font
-            font.setKerning_(currentLightFont.kerning(), newMasterID)
+# # set custom param "Axes"
+#     # Weight, wght
+#     # Negative, NEGA
 
-                ## these need to be layer indexes, it seems
-                # bring glyph data into glyph of new master
-                # glyph.layers()[newMasterID] = currentGlyph.layers()[currentLightFontMasterID]
+# newCustomParam = GSCustomParameter().init()
 
+# font.addCustomParameter_(newCustomParam)
 
-        # if instance.interpolationWeight() == boldValue:
-        #     currentBoldFont = font.generateInstance_error_(font.instances()[-1], None)
-        #     print(currentBoldFont.fontMasters()[0])
-        #     currentBoldFontMasterID = currentBoldFont.fontMasters()[0].id()
-        #     font.addFontMaster_(currentBoldFont.fontMasters()[0])
-
-        # if instance.interpolationWeight() == lightNegValue or instance.interpolationWeight() == boldNegValue:
-        #     print(instance.name() + " Negative")
-        #     print(instance.interpolationWeight())
+# print(font.customParameterForKey("Axes"))
 
 
+# # font.setCustomParameter_forKey_()
+
+# # set weight value in each master
+#     # if "light" in name, weight = light instance (50)
+#     # if "bold"
+
+# # set negative value in each master
+#     # if "Negative" in name, Negative = -1
+#     # else Negative = 0
+
+# # set weight value in each instance
+#     # if "light" in name, weight = light instance (50)
+#     # if "bold"
+
+# # set negative value in each instance
+#     # if "Negative" in name, Negative = -1
+#     # else Negative = 0
 
 
-# make masters from instance interpolations of:
-    # light
-    # bold
-    # light negative
-    # bold negative
-
-# set custom param "Axes"
-    # Weight, wght
-    # Negative, NEGA
-
-# set weight value in each master
-    # if "light" in name, weight = light instance (50)
-    # if "bold"
-
-# set negative value in each master
-    # if "Negative" in name, Negative = -1
-    # else Negative = 0
-
-# set weight value in each instance
-    # if "light" in name, weight = light instance (50)
-    # if "bold"
-
-# set negative value in each instance
-    # if "Negative" in name, Negative = -1
-    # else Negative = 0
-
-
-# document.close()
-font.save(fullPath)
+# # document.close()
+font.save(buildPath)
 doc.close()
